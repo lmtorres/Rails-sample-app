f960f62b3f03656054bc7c1a37ce05105573164e
Luis Torres <luis@dreamfactorypr.com>
2014-04-04 04:22:35 +0200

    Javascript Turbolinks compatible

--- app/assets/javascripts/charcount.js
+++ app/assets/javascripts/charcount.js
@@ -1,5 +1,17 @@
-$(window).load(function() {
+var ready;
+ready = function() {
 	document.getElementById('micropost_content').onkeyup = function() {
-		document.getElementById('count').innerHTML = (140 - this.value.length) + " characters left";
+		var count = (140 - this.value.length);
+		if (count < 0){
+			document.getElementById('count').style.color = "#ff0000";
+			document.getElementById('micropost_content').style.border = "1px solid #b94a48";
+		} else{
+			document.getElementById('count').style.color = "#000000";
+			document.getElementById('micropost_content').style.border = "1px solid #bbb";
+		}
+		document.getElementById('count').innerHTML = count + " characters left";
 	};
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
